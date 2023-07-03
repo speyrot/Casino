5709c90c09289efed4b387fc25fa7a02144e6175
TKYOpartner <128873901+TKYOpartner@users.noreply.github.com>
2023-07-03 18:31:07 -0400

    Set up and process write out

---
+++ project.js
@@ -0,0 +1,7 @@
+//Step 1. Deposit money
+//Step 2. Determine number of lines to bet on
+//Step 3. Collect bet amount
+//Step 4. Spin slot machine
+//Step 5. Check if user won
+//Step 6. Give user payout
+// Step 7. Play Again
