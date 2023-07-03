980ece8918e5e2746a94ea1a0e1cbfae9a98c7bd
TKYOpartner <128873901+TKYOpartner@users.noreply.github.com>
2023-07-03 18:42:33 -0400

    step 1

--- project.js
+++ project.js
@@ -5,3 +5,25 @@
 //Step 5. Check if user won
 //Step 6. Give user payout
 // Step 7. Play Again
+
+//Beg of Step 1
+
+const prompt = require("prompt-sync") ();
+
+const deposit = () => {
+    while(true) {
+        const depositAmount = prompt("Enter deposit amount: ");
+        const numberDepositAmount = parseFloat(depositAmount);
+    
+        if (isNaN(numberDepositAmount) || numberDepositAmount <= 0) {
+            console.log("Invalid deposit amount, try again.");
+        } else {
+            return numberDepositAmount;
+        }    
+    }
+};
+
+const depositAmount = deposit();
+console.log(depositAmount);
+
+//End of Step 1
