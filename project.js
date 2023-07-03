7e6e7389a5d93682e507c7089c8c354f6a83b3a3
TKYOpartner <128873901+TKYOpartner@users.noreply.github.com>
2023-07-03 18:57:52 -0400

    Step 2

--- project.js
+++ project.js
@@ -27,3 +27,23 @@ const depositAmount = deposit();
 console.log(depositAmount);
 
 //End of Step 1
+
+//Beg of Step 2
+
+const getNumberOfLines = () => {
+    while(true) {
+        const lines = prompt("Enter number of lines to bet on (1-3): ");
+        const numberOfLines = parseFloat(lines);
+    
+        if (isNaN(numberOfLines) || numberOfLines <= 0 || numberOfLines > 3) {
+            console.log("Invalid number of lines, try again.");
+        } else {
+            return numberOfLines;
+        }    
+    }
+};
+
+const lines = getNumberOfLines();
+console.log(lines);
+
+//End of Step 2
